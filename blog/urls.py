8570008ed79db52d9af73224588555ca0d870f1b
Harshdeep Singh <harshdeepsinghofficial2005@gmail.com>
2021-06-09 20:31:06 +0530

    Renders different blogs in blogpost.html

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='blogHome'),
-    path('blogpost/', views.blogpost, name='blogPost'),
+    path('blogpost/<int:id>', views.blogpost, name='blogPost'),
 ]
